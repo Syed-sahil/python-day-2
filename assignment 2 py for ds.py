28e7ed88b4999e8286083aededd9f32658ce71d3
Syed-sahil <66295936+Syed-sahil@users.noreply.github.com>
2021-09-24 13:23:33 +0530

    Add files via upload

---
+++ assignment 2 py for ds.py
@@ -0,0 +1,25 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+#Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.ess Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+#
+
+
+           # n=int(input("enter the size of the list"))
+           # a[]
+           # for i in range(num)
+             #   val=int(input("enter the number"))
+              #  a.append(val)
+
+list = [10, 21, 4, 45, 66, 93,54,65,78]
+
+# iterating each number in list
+
+for num in list:
+
+    # checking condition
+
+
+    if num % 2 == 0:
+        print(num, end=" ")
+
